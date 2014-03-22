e1eb4fde11a555480e722bc4fa65f7454cdd9c9e
Jeroen Lodder <lodderj@gmail.com>
2014-03-22 18:29:51 +0100

    add license

--- test/xIFO_tester_cm4/main.cpp
+++ test/xIFO_tester_cm4/main.cpp
@@ -1,12 +1,36 @@
+/* 
+ * Copyright (c) 2014 Jeroen Lodder
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "mbed.h"
 
+Serial pc(SERIAL_TX, SERIAL_RX);
+ 
 DigitalOut myled(LED1);
-
+ 
 int main() {
-    while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
-    }
+  int i = 1;
+  pc.printf("Hello World !\n");
+  while(1) { 
+      wait(1);
+      pc.printf("This program runs since %d seconds.\n", i++);
+      myled = !myled;
+  }
 }
--- xIFO/xIFO.cpp
+++ xIFO/xIFO.cpp
@@ -3,10 +3,31 @@
  * @brief   xifo circular buffer with <t>/8/16/32/64 bit elements
  * @details xifo supplies object oriented circular buffer with 8 bit size elements. \n
  * 			To use either as FIFO (First In First Out) or as FILO (First In Last Out)
+ * 			You might want to rename this file is you are using a C compiler.
  *
  * @Author	Jeroen Lodder
- * @Date	April 2013
+ * @Date	March 2014
  * @version 3
+ * 
+ * Copyright (c) 2014 Jeroen Lodder
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * @{
  */
